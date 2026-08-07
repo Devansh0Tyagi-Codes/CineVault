d455f1637ea7b5dfbd4a46d85eb5fc979ce7516d
Devansh0Tyagi-Codes <devansh.25b01011314@abes.ac.in>
2026-08-07 21:07:10 +0530

    Learned DOM selection using querySelector

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("CineVault Loaded 🚀");
+
+// Variables
+
+let websiteName = "CineVault";
+
+let totalMovies = 4;
+
+let totalWebShows = 4;
+
+console.log(websiteName);
+
+console.log(totalMovies);
+
+console.log(totalWebShows);
+
+// ==========================
+// DOM
+// ==========================
+
+// Hero Section
+
+const heroSection = document.querySelector(".hero");
+
+console.log(heroSection);
+
+const movieSection = document.querySelector(".trending");
+
+console.log(movieSection);
+
+const footer = document.querySelector("footer");
+
+console.log(footer);
