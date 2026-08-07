f0cb859e5aedd5a90ffce1874c82eca065513e07
Devansh0Tyagi-Codes <devansh.25b01011314@abes.ac.in>
2026-08-07 21:17:43 +0530

    Updated hero content using JavaScript DOM

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ console.log(totalWebShows);
 const heroSection = document.querySelector(".hero");
 
 console.log(heroSection);
+heroSection.style.color = "yellow";
+
+
+
+const heroHeading = document.querySelector(".hero-content h1");
+
+console.log(heroHeading);
+heroHeading.textContent = "Explore Movies & Web Shows 🍿";
+
+const heroPara = document.querySelector(".hero-content p");
+heroPara.textContent = "Find your next binge-worthy movie or web series.";
 
 const movieSection = document.querySelector(".trending");
 
